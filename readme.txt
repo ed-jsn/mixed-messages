444a2d7babd6076a085fb8588af609f128ccecbe
ed-jsn <edward.jackson15@bathspa.ac.uk>
2021-02-04 17:20:20 +0000

    version 1.0.0 commit.

--- readme.txt
+++ readme.txt
@@ -1,1 +1,6 @@
-mixed messages project
+**** Andrei Tarkovsky Quotebot version 1.0.0 ****
+
+This script gives a random quote from the cinema maestro Andrei Tarkovsky when run.
+It also gives a film recommendation and another great director to explore.
+
+Have Fun!
--- script.js
+++ script.js
@@ -1,3 +1,178 @@
-let a = 5;
-let b = 5;
-console.log(a + b);
+const data = {
+    quotes: [
+        `
+        “Through poetic connections feeling is heightened and the spectator is made more active. 
+        He becomes a participant in the process of discovering life, unsupported by ready-made 
+        deductions from the plot or ineluctable pointers by the author. He has at his disposal 
+        only what helps to penetrate to the deeper meaning of the complex phenomena represented 
+        in front of him. Complexities of thought and poetic visions of the world do not have to 
+        be thrust into the framework of the patently obvious. The usual logic, that of linear 
+        sequentiality, is uncomfortably like the proof of a geometry theorem. As a method it is 
+        incomparably less fruitful artistically than the possibilities opened up by associative 
+        linking, which allows for an affective as well as a rational appraisal. And how wrong it 
+        is that the cinema makes so little use of the latter mode, which has so much to offer. 
+        It possesses an inner power which is concentrated within the image and comes across to 
+        the audience in the form of feelings, inducing tension in direct response to the author’s 
+        narrative logic.”
+        (From Sculpting in Time: Reflections on the Cinema, page 20)
+
+        `,
+        
+        `
+        “Everything depends on how extensively artists sense this responsibility within themselves. 
+        How can one demand this of others! Even the smallest demand would be indiscreet. Therefore 
+        I think that it makes sense to talk about something related, yet a bit different: the role
+        of art in contemporary life. Of course, it's very clear that in our time art finds itself
+        in a state of deep depression. This is not something that depends upon various social 
+        conditions and circumstances, or on the public's lack of interest. The roots of this problem 
+        lie elsewhere. For the public is interested in art, and artists search for the path and 
+        possibility of communicating with the public, yet both public and artist suffer from 
+        insufficient contact and insufficient potential for interaction. Those who are closely 
+        connected with art understand these problems. Apparently, the problem is that art is 
+        becoming less and less spiritual. It has already set its sights on something completely 
+        different, and this is not something it needs to seek out or strive for. And to a large 
+        degree, this hinges on the public, which in the end influences that which transpires in 
+        art.”
+        (Andrei Tarkovsky interviewed by V. Ishimov and R. Shejko, 1984)
+
+        `, 
+        
+        `
+        “As I see it then, if you stand on firm moral ground there is no need to shy away from 
+        greater freedom in your choice of means. Moreover, that freedom need not necessarily be
+        restricted to a clear plan which obliges you to choose between certain methods. You also
+        have to be able to trust solutions which present themselves spontaneously. Obviously it 
+        is important that these should not put the audience off by being overcomplex. This, 
+        however, is not something to be gauged by deliberations about what devices to ban or allow
+        in your film, but through the experience gained by looking at the excesses that found their
+        way into your early productions and which have to be eliminated naturally as your work 
+        proceeds.” 
+        (From Sculpting in Time: Reflections on the Cinema, page 27)
+
+        `, 
+        
+        `
+        “Time and memory merge into each other; they are like the two sides of a medal. It is 
+        obvious enough that without Time, memory cannot exist either. But memory is something 
+        so complex that no list of all its attributes could define the totality of the impressions
+        through which it affects us. Memory is a spiritual concept! For instance, if somebody 
+        tells us of his impressions of childhood, we can say with certainty that we shall have 
+        enough material in our hands to form a complete picture of that person. Bereft of memory, 
+        a person becomes the prisoner of an illusory existence; falling out of time he is unable
+        to seize his own link with the outside world—in other words he is doomed to madness. As
+        a moral bring, man is endowed with memory which sows in him a sense of dissatisfaction. 
+        It makes us vulnerable, subject to pain.” 
+        (From Sculpting in Time: Reflections on the Cinema, pages 57 and 58)
+
+        `, 
+        
+        `
+        “Audiences have become more discerning in their attitude to films. Cinema as such long 
+        ago ceased to amaze them as a new and original phenomenon; and at the same time it is 
+        expected to answer a far wider range of individual needs. Audiences have developed their
+        likes and dislikes. That means that the film-maker in turn has an audience that is 
+        constant, his own circle. Divergence of taste on the part of audiences can be extreme, 
+        and this is in no way regrettable or alarming; the fact that people have their own 
+        aesthetic criteria indicates a growth of self-awareness. Directors are going deeper 
+        into the areas which concern them. There are faithful audiences and favorite directors, 
+        so that there is no question of thinking in terms of unqualified success with the 
+        public—that is, if one is talking about cinema not as entertainment but as art. Indeed, 
+        mass popularity suggests what is known as mass culture, and not art.” 
+        (From Sculpting in Time: Reflections on the Cinema, page 84)
+
+        `, 
+        
+        `
+        “If we round off its date of birth, cinema can be said to be contemporary with the 
+        twentieth century. That is no accident. It means that rather over eighty years ago the
+        point was reached when a new muse had to emerge. Cinema was the first art form to come
+        into being as a result of a technological invention, in answer to a vital need. It was
+        the instrument which humanity had to have in order to increase its mastery over the 
+        real world. For the domain of any art form is limited to one aspect of our spiritual 
+        and emotional discovery of surrounding reality. As he buys a ticket, it’s as if the 
+        cinema-goer were seeking to make up for the gaps in his own experience, throwing 
+        himself into a search for ‘lost time.’ In other words he seeks to fill that spiritual
+        vacuum which has formed as a result of the specific conditions of his modern existence:
+        constant activity, curtailment of human contact, and the materialist bent of modern 
+        education.” 
+        (From Sculpting in Time: Reflections on the Cinema, pages 82 and 83)
+
+        `, 
+        
+        `
+        “It's impossible to create something unreal. Everything is real and unfortunately 
+        we aren't able to abandon reality. We can express ourselves toward the world that 
+        exists in a poetic way or a purely descriptive manner. Personally, I prefer to 
+        express myself in a metaphoric way. I insist on saying metaphoric and not symbolic.
+        The symbol intrinsically comprises a specific meaning, an intellectual formula, while
+        the metaphor is the image itself. It's an image that possesses the same characteristics
+        as the world it represents. Contrary to the symbol, its meaning is undefined. We 
+        aren't able to speak about a world that is truly boundless utilizing means which 
+        themselves are deflnite and restricted. We can analyze a formula, that is to say, 
+        a symbol, but a metaphor is an entity unto itself, a monomial. If one tries to describe
+        it, immediately it falls to pieces.” 
+        (Andrei Tarkovsky interviewed by Hervé Guibert, 1983)
+
+        `, 
+        
+        `
+        “The meaning of religious truth is hope. Philosophy seeks the truth, defining the 
+        meaning of human activity, the limits of human reason, the meaning of existence, 
+        even when the philosopher reaches the conclusion that existence is senseless, and
+        human effort—futile. The allotted function of art is not, as is often assumed, to
+        put across ideas, to propagate thoughts, to serve as example. The aim of art is 
+        to prepare a person for death, to plough and harrow his soul, rendering it capable
+        of turning to good. Touched by a masterpiece, a person begins to hear in himself 
+        that same call of truth which prompted the artist to his creative act. When a link
+        is established between the work and its beholder, the latter experiences a 
+        sublime, purging trauma. Within that aura which unites masterpieces and audience, 
+        the best sides of our souls are made known, and we long for them to be freed. In 
+        those moments we recognize and discover ourselves, the unfathomable depths of our 
+        own potential, and the furthest reaches of our emotions. 
+        (From Sculpting in Time: Reflections on the Cinema, page 43)
+
+        `
+    ],
+    films: [
+        `The Steamroller and the Violin (1961)`,
+        `Ivan's Childhood (1962)`,
+        `Andrei Rublev (1966)`,
+        `Solaris (1972)`,
+        `The Mirror (1975)`,
+        `Stalker (1979)`,
+        `Nostalgia (1983)`,
+        `The Sacrifice (1986)`
+    ],
+    directors: [
+        `Luis Buñuel`,
+        `Kenji Mizoguchi`,
+        `Ingmar Bergman`,       
+        `Robert Bresson`,
+        `Akira Kurosawa`,
+        `Michelangelo Antonioni`,
+        `Frederico Fellini`,
+        `Sergei Parajanov`
+    ]
+
+}
+
+function random() {x = (Math.floor(Math.random()* 8)); return x}
+
+quoteNum = random();
+filmNum = random();
+directorNum = random();
+
+quoteReturn = data.quotes[quoteNum]
+filmReturn = data.films[filmNum]
+directorReturn = data.directors[directorNum]
+
+console.log
+(
+    `**** Andrei Tarkovsky Quotebot version 1.0.0 **** 
+    Andrei Says: 
+    ${quoteReturn} 
+    Andrei recommends his film : ${filmReturn}.
+    Andrei also insists that you investigate the work of ${directorReturn}. 
+    Andrei wishes you a pleasant day.`
+)
+;
